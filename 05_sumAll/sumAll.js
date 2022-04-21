773709edef0e739673791fab2f8468058a6d4787
Ryan Zubery <ryan.zubery@icloud.com>
2022-04-20 19:17:44 -0600

    Add solution for 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    if(typeof num1 !== 'number' || typeof num2 !== 'number') {
+        return 'ERROR';
+    } else if (num1 < 0 || num2 < 0) {
+        return 'ERROR';
+    }
 
+    const first = Math.min(num1, num2);
+    const last = Math.max(num1, num2);
+    const totalInts = last - first + 1;
+    const sum = ((first + last) * totalInts) / 2;
+    return sum;
 };
 
 // Do not edit below this line
