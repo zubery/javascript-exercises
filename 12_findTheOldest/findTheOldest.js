dc895f9d140965f9455442e5f82f4eace22647f5
Ryan Zubery <ryan.zubery@icloud.com>
2022-04-21 10:08:40 -0600

    Add solution for 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
+const findTheOldestHelper = function(person1, person2) {
+    const person1FirstYear = person1.yearOfBirth;
+    const person1LastYear = person1.yearOfDeath ?? (new Date()).getFullYear();
+    const person1Age = person1LastYear - person1FirstYear;
 
+    const person2FirstYear = person2.yearOfBirth;
+    const person2LastYear = person2.yearOfDeath ?? (new Date()).getFullYear();
+    const person2Age = person2LastYear - person2FirstYear;
+
+    return person1Age > person2Age ? person1 : person2;
+};
+
+const findTheOldest = function(people) {
+    return people.reduce(findTheOldestHelper, { yearOfBirth: 0, yearOfDeath: 0 });
 };
 
 // Do not edit below this line
