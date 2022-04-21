3b6a5d7ad7879689a369cf65527a28ee8993e2f8
Ryan Zubery <ryan.zubery@icloud.com>
2022-04-20 19:27:18 -0600

    Add solution for 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,15 @@
-const ftoc = function() {
+const roundToOneDecimal = function(number) {
+  return Math.round(number * 10) / 10;
+}
 
+const ftoc = function(fahrenheit) {
+  const celcius = (fahrenheit - 32) * (5/9);
+  return roundToOneDecimal(celcius);
 };
 
-const ctof = function() {
-
+const ctof = function(celcius) {
+  const fahrenheit = (celcius * (9/5)) + 32;
+  return roundToOneDecimal(fahrenheit);
 };
 
 // Do not edit below this line
