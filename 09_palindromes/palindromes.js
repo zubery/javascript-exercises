38682e95f4966c51bcea4e9c3a0333dd7ea60e9e
Ryan Zubery <ryan.zubery@icloud.com>
2022-04-21 09:45:38 -0600

    Add solution for 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function () {
+const lowerLettersString = function(string) {
+    return string.replaceAll(/[^A-Za-z]/g, '').toLowerCase();
+}
 
+const palindromes = function (string) {
+    const processedString = lowerLettersString(string);
+    const processedLength = processedString.length;
+    const numLetterPairs = Math.floor(processedLength / 2);
+    for(let i = 0; i < numLetterPairs; i++) {
+        if(processedString[i] !== processedString[processedLength - i - 1]) {
+            return false;
+        }
+    }
+    return true;
 };
 
 // Do not edit below this line
