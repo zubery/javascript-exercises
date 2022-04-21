efeae085b8e0220360e7fef5bf4ea9b4177e5d60
Ryan Zubery <ryan.zubery@icloud.com>
2022-04-21 09:50:52 -0600

    Add solution for 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,11 @@
-const fibonacci = function() {
-
+const fibonacci = function(inputNum) {
+    const num = Number(inputNum);
+    if(num < 1) {
+        return 'OOPS';
+    } else if(num === 1 || num === 2) {
+        return 1;
+    }
+    return fibonacci(num - 1) + fibonacci(num - 2);
 };
 
 // Do not edit below this line
