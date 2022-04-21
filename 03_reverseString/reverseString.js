02c9949b20d6088e26226f33aef3e226d01c1c1d
Ryan Zubery <ryan.zubery@icloud.com>
2022-04-20 18:57:54 -0600

    Add solution for 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,6 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    const stringArray = [...string];
+    return stringArray.reverse().join('');
 };
 
 // Do not edit below this line
