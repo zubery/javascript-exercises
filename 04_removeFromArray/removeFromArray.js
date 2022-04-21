9be406b2cc20a785c7eebc6fca975477bbfac066
Ryan Zubery <ryan.zubery@icloud.com>
2022-04-20 19:09:04 -0600

    Add solution for 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,7 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...removeArgs) {
+    return array.filter(
+        element => !removeArgs.includes(element)
+    );
 };
 
 // Do not edit below this line
